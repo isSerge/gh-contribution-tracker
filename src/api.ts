5ef7508cfc6361dc7bd336412324153bccd7bc7a
Serge Kovbasiuk <isserge@protonmail.com>
2023-05-24 10:51:04 +0400

    fetch contributions using GraphQL api

---
+++ src/api.ts
@@ -0,0 +1,75 @@
+import { graphql } from '@octokit/graphql';
+import { config } from 'dotenv';
+
+config();
+
+const githubToken = process.env.GITHUB_TOKEN;
+
+const graphqlWithAuth = graphql.defaults({
+  headers: {
+    authorization: `token ${githubToken}`,
+  },
+});
+
+async function fetchOrganizationId(org: string) {
+  const query = `
+    query ($org: String!) {
+      organization(login: $org) {
+        id
+      }
+    }
+  `;
+
+  const result = await graphqlWithAuth(query, { org });
+
+  return (result as any).organization.id;
+}
+
+export async function fetchUserContributions(org: string, username: string, startDate: Date, endDate: Date) {
+  const organizationId = await fetchOrganizationId(org);
+
+  const query = `
+    query ($username: String!, $orgId: ID!, $from: DateTime!, $to: DateTime!) {
+      user(login: $username) {
+        contributionsCollection(organizationID: $orgId, from: $from, to: $to) {
+          startedAt
+          endedAt
+          hasAnyContributions
+          hasActivityInThePast
+          issueContributions(first: 100) {
+            nodes {
+              issue {
+                title
+                createdAt
+                repository {
+                  name
+                }
+              }
+            }
+          }
+          pullRequestContributions(first: 100) {
+            nodes {
+              pullRequest {
+                title
+                createdAt
+                merged
+                repository {
+                  name
+                }
+              }
+            }
+          }
+        }
+      }
+    }  
+  `;
+
+  const result = await graphqlWithAuth(query, {
+    username,
+    orgId: organizationId,
+    from: startDate.toISOString(),
+    to: endDate.toISOString(),
+  });
+
+  return result;
+}
