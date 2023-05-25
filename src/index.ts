9c7247d1bee891db7b5552531ca3bee98c3908b8
Serge Kovbasiuk <isserge@protonmail.com>
2023-05-25 08:28:52 +0400

    fetch name and handle from Notion

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,65 @@
 import { fetchUserContributions } from "./api"
+import { graphql } from '@octokit/graphql';
+import { OpenAI } from "langchain/llms/openai";
+import { config } from 'dotenv';
+import { Client } from '@notionhq/client';
+
 import { formatAsPromptInput } from "./format";
 import { getContributionSummary } from "./langchain";
-import { Contributions } from './types';
+import { Contributions, isTupleStringArray } from './types';
+import { getNamesAndHandles } from './notion';
 
-export async function main(username: string, startDateInput?: Date, endDateInput?: Date) {
+config();
+
+const githubToken = process.env.GITHUB_TOKEN;
+
+const graphqlClient = graphql.defaults({
+  headers: {
+    authorization: `token ${githubToken}`,
+  },
+});
+
+
+const openAIApiKey = process.env.OPENAI_API_KEY;
+
+const model = new OpenAI({
+  openAIApiKey,
+  temperature: 0.9,
+  modelName: "gpt-3.5-turbo",
+  // modelName: "gpt-4",
+  streaming: true,
+  callbacks: [
+    {
+      handleLLMNewToken(token: string) {
+        process.stdout.write(token);
+      },
+    },
+  ],
+});
+
+const notionApiKey = process.env.NOTION_API_KEY;
+const databaseId = process.env.NOTION_DATABASE_ID as string;
+
+const notion = new Client({ auth: notionApiKey });
+
+export async function main(
+  startDateInput?: Date, endDateInput?: Date
+) {
   const endDate = endDateInput || new Date();
   const startDate = startDateInput || new Date(endDate.getFullYear(), endDate.getMonth(), endDate.getDate() - 14);
+  const tuples = await getNamesAndHandles(notion, databaseId);
 
-  const contributions = await fetchUserContributions('subspace', username, startDate, endDate) as Contributions;
-  const formattedContributions = formatAsPromptInput(contributions);
+  // TODO: handle this properly
+  if (!isTupleStringArray(tuples)) return;
 
-  const summary = await getContributionSummary(formattedContributions);
+  for (const [name, githubHandle] of tuples) {
+    const contributions = await fetchUserContributions(graphqlClient, 'subspace', githubHandle, startDate, endDate) as Contributions;
+    const formattedContributions = formatAsPromptInput(contributions);
+    const summary = await getContributionSummary(model, formattedContributions);
 
-  console.log(summary);
+    // TODO: Add a notion page for the user and add the summary to it.
+  }
 }
 
-main('isSerge');
+main();
 
--- src/types.ts
+++ src/types.ts
@@ -37,3 +37,9 @@ export function isNodeIssue(node: NodeIssue | NodePR): node is NodeIssue {
 export function isNodePR(node: NodeIssue | NodePR): node is NodePR {
   return 'pullRequest' in node;
 }
+
+type TupleStringArray = [string, string][];
+
+export function isTupleStringArray(value: any): value is TupleStringArray {
+    return Array.isArray(value) && value.every(item => Array.isArray(item) && item.length === 2 && typeof item[0] === 'string' && typeof item[1] === 'string');
+}
